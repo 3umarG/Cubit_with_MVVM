ee1fce3226732897a02f10d41f2dfe0758b7e9c4
OmarGomaaFCi <omargomaafcitu@gmail.com>
2023-04-15 17:39:33 +0200

    Build Cubit Logic and States

--- lib/business/cubit/characters_cubit.dart
+++ lib/business/cubit/characters_cubit.dart
@@ -1,8 +1,40 @@
 import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
+import 'package:cubit_tutorial/core/exception_handling/failures.dart';
+import 'package:flutter/material.dart';
+
+import '../../data/models/character_model.dart';
+import '../../data/repository/characters_repo.dart';
 
 part 'characters_state.dart';
 
 class CharactersCubit extends Cubit<CharactersState> {
-  CharactersCubit() : super(CharactersInitial());
+  final CharactersRepository repo;
+  late List<CharacterModel> characters;
+  CharactersCubit(this.repo) : super(CharactersInitial());
+
+  Future<void> fetchAllCharacters() async {
+    emit(CharactersStateLoading());
+    final result = await repo.fetchAllCharacters();
+
+    result.fold(
+      (failure) {
+        switch(failure.runtimeType) {
+          case ServerFailure:
+            emit(CharactersStateErrorServer(failure.errorObject.message));
+            break;
+          case NoConnectionFailure:
+            emit(CharactersStateErrorNoConnection(failure.errorObject.message));
+            break;
+          case DataParsingFailure:
+            emit(CharactersStateErrorDataParsing(failure.errorObject.message));
+            break;
+        }
+
+      },
+      (characters) {
+        this.characters = characters;
+        emit(CharactersStateLoaded(characters));
+      },
+    );
+  }
 }
--- lib/business/cubit/characters_state.dart
+++ lib/business/cubit/characters_state.dart
@@ -3,4 +3,30 @@ part of 'characters_cubit.dart';
 @immutable
 abstract class CharactersState {}
 
+abstract class CharactersStateError extends CharactersState {
+  final String message;
+
+  CharactersStateError(this.message);
+}
+
 class CharactersInitial extends CharactersState {}
+
+class CharactersStateLoaded extends CharactersState {
+  final List<CharacterModel> characters;
+
+  CharactersStateLoaded(this.characters);
+}
+
+class CharactersStateErrorServer extends CharactersStateError {
+  CharactersStateErrorServer(super.message);
+}
+
+class CharactersStateErrorDataParsing extends CharactersStateError {
+  CharactersStateErrorDataParsing(super.message);
+}
+
+class CharactersStateErrorNoConnection extends CharactersStateError {
+  CharactersStateErrorNoConnection(super.message);
+}
+
+class CharactersStateLoading extends CharactersState{}
